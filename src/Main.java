6122c663ffe3f08909ddacdb32bf4ab78c4140d5
charlesanderson25 <charles_spfc@hotmail.com>
2025-02-11 17:43:27 -0300

    First commit, declaração de variáveis

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Declaração de variáveis
+
+        int numero = 25;
+        double decimal = 25.25;
+
+        int idade;
+        idade = 25;
+
+        String idadeString;
+        idadeString = "25 anos";
+        System.out.println(
+                "Criação de variáveis:\n" +
+                        "numero: " + numero + "\n" +
+                        "decimal: " + decimal + "\n" +
+                        "idade: " + idade + "\n" +
+                        "idadeString: " + idadeString + "\n"
+        );
+
+        var nome = "Charles";
+        System.out.println("O valor da variável é: " + nome);
+        nome = "Gécica";
+        System.out.println("O valor da variável é: " + nome);
+
+
+
+    }
+}
