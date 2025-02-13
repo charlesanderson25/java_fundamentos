f84c67b8410a36fec82c7a0d14f5e96f3c6c71c4
charlesanderson25 <charles_spfc@hotmail.com>
2025-02-13 18:19:56 -0300

    Loops file

---
+++ src/Loops.java
@@ -0,0 +1,17 @@
+public class Loops {
+    public static void main(String[] args) {
+        // Loops
+
+        // For
+        for (var i = 0; i < 5; i++){
+            System.out.println("Usando o FOR");
+        }
+
+        // While
+        int i = 0;
+        while (i < 5){
+            i++;
+            System.out.println("Imprimindo o while");
+        }
+    }
+}
